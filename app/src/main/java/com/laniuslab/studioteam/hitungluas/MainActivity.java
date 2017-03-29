aaef38656d6890b51a86912056abda2c544569ca
Agam <agammail95@gmail.com>
2017-03-29 11:33:20 +0700

    INITIAL_COMMIT

---
+++ app/src/main/java/com/laniuslab/studioteam/hitungluas/MainActivity.java
@@ -0,0 +1,61 @@
+package com.laniuslab.studioteam.hitungluas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
+
+        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
+        edtLebar = (EditText)findViewById(R.id.edt_lebar);
+        btnHitung = (Button)findViewById(R.id.btn_hitung);
+        txtLuas = (TextView)findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.btn_hitung:
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+
+                if(TextUtils.isEmpty(panjang) || TextUtils.isEmpty(lebar)){
+                    Toast.makeText(this,"Nilai yang dimasukkan tidak boleh kosong",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.equals(panjang,".")||TextUtils.equals(lebar,".")){
+                    Toast.makeText(this,"Nilai yang dimasukkan tidak boleh hanya titik",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                double p = Double.parseDouble(panjang);
+                double l = Double.parseDouble(lebar);
+
+                double luas = p * l;
+
+                txtLuas.setText("Luas : "+luas);
+
+                break;
+        }
+    }
+
+
+
+}
